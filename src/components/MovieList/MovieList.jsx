58532b60347008d193898403aa793494483859a5
Maria Kasycheva <167572285+Kasycheva@users.noreply.github.com>
2024-11-22 11:00:39 +0100

    Fixed useLocation implementation in MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom'; 
 import styles from './MovieList.module.css';
 
 const MovieList = ({ movies }) => {
+  const location = useLocation();
   const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
   return (
     <ul className={styles.movieList}>
       {movies.map((movie) => (
         <li key={movie.id} className={styles.movieItem}>
-          <Link to={`/movies/${movie.id}`} className={styles.movieLink}>
+          <Link 
+            to={`/movies/${movie.id}`} 
+            state={{ from: location }}
+            className={styles.movieLink}
+          >
             <img
               src={movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : 'https://via.placeholder.com/200x300'}
               alt={movie.title}
@@ -35,4 +40,3 @@ MovieList.propTypes = {
 };
 
 export default MovieList;
-
